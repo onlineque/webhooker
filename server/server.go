01166d87fcf8658ab555e9a60bac76b3d96d23cd
Vladimir Siman <online@clew.cz>
2024-03-12 21:48:51 +0100

    request checking and validation

--- server/server.go
+++ server/server.go
@@ -1,9 +1,21 @@
 package server
 
 import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"github.com/go-playground/validator/v10"
+	"io"
 	"log"
 	"net/http"
+	"strings"
 )
+
+type WebhookerRequest struct {
+	Token   string `json:"token" validate:"required"`
+	Channel string `json:"channel" validate:"required"`
+	Message string `json:"message" validate:"required"`
+}
 
 func Listen(address string, certFile string, keyFile string) error {
 	http.HandleFunc("/webhook", handleWebhook)
@@ -12,5 +24,65 @@ func Listen(address string, certFile string, keyFile string) error {
 }
 
 func handleWebhook(w http.ResponseWriter, r *http.Request) {
-	log.Print("webhook received")
+	ct := r.Header.Get("Content-Type")
+	mediaType := strings.ToLower(strings.TrimSpace(strings.Split(ct, ";")[0]))
+	if mediaType != "application/json" {
+		msg := "Content-Type header set to application/json is required"
+		http.Error(w, msg, http.StatusUnsupportedMediaType)
+		return
+	}
+
+	r.Body = http.MaxBytesReader(w, r.Body, 1048576)
+
+	dec := json.NewDecoder(r.Body)
+	dec.DisallowUnknownFields()
+
+	var wr WebhookerRequest
+	err := dec.Decode(&wr)
+	if err != nil {
+		var syntaxError *json.SyntaxError
+		var unmarshalTypeError *json.UnmarshalTypeError
+
+		switch {
+		case errors.As(err, &syntaxError):
+			msg := fmt.Sprintf("Request body contains badly-formatted JSON (at position %d)", syntaxError.Offset)
+			http.Error(w, msg, http.StatusBadRequest)
+
+		case errors.Is(err, io.ErrUnexpectedEOF):
+			msg := fmt.Sprintf("Request body contains badly-formed JSON")
+			http.Error(w, msg, http.StatusBadRequest)
+
+		case errors.As(err, &unmarshalTypeError):
+			msg := fmt.Sprintf("Request body contains an invalid value for the %q field (at position %d)", unmarshalTypeError.Field, unmarshalTypeError.Offset)
+			http.Error(w, msg, http.StatusBadRequest)
+
+		case strings.HasPrefix(err.Error(), "json: unknown field "):
+			fieldName := strings.TrimPrefix(err.Error(), "json: unknown field ")
+			msg := fmt.Sprintf("Request body contains unknown field %s", fieldName)
+			http.Error(w, msg, http.StatusBadRequest)
+
+		case errors.Is(err, io.EOF):
+			msg := "Request body must not be empty"
+			http.Error(w, msg, http.StatusBadRequest)
+
+		case err.Error() == "http: request body too large":
+			msg := "Request body must not be larger than 1MB"
+			http.Error(w, msg, http.StatusRequestEntityTooLarge)
+		default:
+			log.Print(err.Error())
+			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		}
+		return
+	}
+
+	validate := validator.New()
+	err = validate.Struct(wr)
+	if err != nil {
+		log.Print(err)
+		msg := fmt.Sprintf("failed to validate struct - %s", err)
+		http.Error(w, msg, http.StatusBadRequest)
+	}
+
+	log.Printf("webhook request: %+v", wr)
+	http.Error(w, "everything's all right", http.StatusOK)
 }
